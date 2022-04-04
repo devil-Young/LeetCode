e8bc280f82512729f95335c0f5a1b3048fd7a687
Young <young.you.bi@gmail.com>
2022-04-04 18:07:14 +0800

    add q5 soultion 2

--- src/q5_最长回文子串/Solution.java
+++ src/q5_最长回文子串/Solution.java
@@ -37,7 +37,7 @@ public class Solution {
                 if(charArray[i] != charArray[j]){
                     dp[i][j] = false;
                 }else {
-                    if (l <= 2 ){
+                    if (j - i <3){
                         dp[i][j] = true;
                     }else {
                         dp[i][j] = dp[i+1][j-1];
@@ -52,5 +52,30 @@ public class Solution {
             }
         }
         return s.substring(begin,begin + maxLen);
+    }
+
+    public String longestPalindrome2(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // 中心扩散的有两种情况 aba   abba
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start){
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+    // 返回长度
+    public int expandAroundCenter(String s, int left, int right){
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        // 多加了
+        return right -left - 1;
     }
 }
