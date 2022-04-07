3439dd75168643025d2afc6187cc009443d2dd0c
Young <young.you.bi@gmail.com>
2022-04-07 17:22:25 +0800

    add q6

--- src/q9_回文数/Solution.java
+++ src/q9_回文数/Solution.java
@@ -6,13 +6,13 @@ package q9_回文数;
 public class Solution {
     // 反转一半数字
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 ==0 && x != 0)){
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 
         int revertedNumber = 0;
-        while (x > revertedNumber){
-            revertedNumber  =  revertedNumber * 10 + x % 10;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
             x /= 10;
         }
         return x == revertedNumber || x == revertedNumber / 10;
