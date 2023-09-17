ac1884e1246e9e4a177dac7e2131d63a08ad9f7b
Young <young.you.bi@gmail.com>
2023-09-17 18:36:48 +0800

    add q19 Solution

---
+++ src/q19_删除链表的倒数第N个结点/Solution.java
@@ -0,0 +1,29 @@
+package q19_删除链表的倒数第N个结点;
+
+/**
+ * @Author: Young
+ * @Date: 2023/9/17 11:39
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        int length = getLength(head);
+        ListNode cur = dummy;
+        for (int i = 1; i < length - n + 1; i++) {
+            cur = cur.next;
+        }
+        // 下一个节点就是要删除的节点
+        cur.next = cur.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+    public int getLength(ListNode head) {
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
+    }
+}
---
+++ src/q19_删除链表的倒数第N个结点/Solution2.java
@@ -0,0 +1,29 @@
+package q19_删除链表的倒数第N个结点;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Author: Young
+ * @Date: 2023/9/17 18:03
+ *
+ * // 栈的方法
+ */
+public class Solution2 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        Deque<ListNode> stack = new LinkedList<>();
+        ListNode cur = dummy;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        for (int i = 0; i < n; i++) {
+            stack.pop();
+        }
+        ListNode prev = stack.peek();
+        prev.next = prev.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+}
