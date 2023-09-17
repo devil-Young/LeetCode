6e60151cb1ae76c61043c7e56aa15c9cc36b3526
Young <young.you.bi@gmail.com>
2023-09-17 22:03:17 +0800

    add q21 Solution

---
+++ src/q21_合并两个有序链表/Solution.java
@@ -0,0 +1,23 @@
+package q21_合并两个有序链表;
+
+/**
+ * @Author: Young
+ * @Date: 2023/9/17 21:25
+ *
+ * 递归
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
