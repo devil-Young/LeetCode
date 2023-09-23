e37fb6c1866bbb32c7fe7ba04e9b59a0821a7aa2
Young <young.you.bi@gmail.com>
2023-09-23 22:11:01 +0800

    add q22 Solution2

---
+++ src/q22_括号生成/Solution2.java
@@ -0,0 +1,36 @@
+package q22_括号生成;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: Young
+ * @Date: 2023/9/23 21:54
+ *
+ * 回溯法
+ * dsf广度优先
+ */
+public class Solution2 {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    private void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
