a9ad79f4c01fee8a463ef5d21a88304f118ce332
Young <young.you.bi@gmail.com>
2023-09-24 22:04:01 +0800

    add q24 Solution2

---
+++ src/q24_两两交换链表中的节点/Solution.java
@@ -0,0 +1,11 @@
+package q24_两两交换链表中的节点;
+
+/**
+ * @Author: Young
+ * @Date: 2023/9/24 20:44
+ *
+ * 递归
+ */
+public class Solution {
+
+}
---
+++ src/q24_两两交换链表中的节点/Solution2.java
@@ -0,0 +1,22 @@
+package q24_两两交换链表中的节点;
+
+/**
+ * @Author: Young
+ * @Date: 2023/9/24 21:40
+ */
+public class Solution2 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+}
