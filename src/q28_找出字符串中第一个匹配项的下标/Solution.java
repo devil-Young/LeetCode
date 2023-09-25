958c4baaecfb405825521fa1065ac6ed6fec4b58
Young <young.you.bi@gmail.com>
2023-09-25 23:28:54 +0800

    add q28 Solution

---
+++ src/q28_找出字符串中第一个匹配项的下标/Solution.java
@@ -0,0 +1,24 @@
+package q28_找出字符串中第一个匹配项的下标;
+
+/**
+ * @Author: Young
+ * @Date: 2023/9/25 22:14
+ */
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
