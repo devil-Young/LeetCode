21818d07391fd89154e01dd8547ac7f2a176550d
Young <young.you.bi@gmail.com>
2023-09-25 21:54:50 +0800

    add q26 Solution

---
+++ src/q26_删除排序数组中的重复项/Solution.java
@@ -0,0 +1,25 @@
+package q26_删除排序数组中的重复项;
+
+/**
+ * @Author: Young
+ * @Date: 2023/9/25 21:32
+ *
+ * 双指针
+ */
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow]  = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
