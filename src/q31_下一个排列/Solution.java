3b40204ad964518e75ffe5f8651420057dd8ce6a
Young <young.you.bi@gmail.com>
2023-11-23 23:18:48 +0800

    add q31 Solution

---
+++ src/q31_下一个排列/Solution.java
@@ -0,0 +1,40 @@
+package q31_下一个排列;
+
+/**
+ * @Author: Young
+ * @Date: 2023/11/23 21:53
+ */
+// 下一个排列也即是不小于当前排列，最接近当前排列的排列
+
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]){
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+}
