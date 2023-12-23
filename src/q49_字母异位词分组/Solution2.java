734455ee5b91f749ae3fa73724b0d2078951a36b
Young <young.you.bi@gmail.com>
2023-12-24 00:42:07 +0800

    add q48 Solution1 and Solution2

---
+++ src/q49_字母异位词分组/Solution2.java
@@ -0,0 +1,39 @@
+package q49_字母异位词分组;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: Young
+ * @Date: 2023/12/24 00:13
+ * 计数
+ * 由于互为字母异位词的两个字符串包含的字母相同，因此两个字符串中的相同字母出现的次数一定是相同的，
+ * 故可以将每个字母出现的次数使用字符串表示，作为哈希表的键。
+ */
+public class Solution2 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] counts = new int[26];
+            int length = str.length();
+            for (int i = 0; i < length; i++) {
+                counts[str.charAt(i) - 'a']++;
+            }
+            // // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串，作为哈希表的键
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                if (counts[i] != 0) {
+                    sb.append((char) 'a' + i);
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
