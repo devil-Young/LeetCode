6ab5f307225eff97e470b29473ddaeb7e442b16b
Young <young.you.bi@gmail.com>
2023-12-27 00:37:22 +0800

    add q33 Solution

---
+++ src/q33_搜索旋转排序数组/Solution.java
@@ -0,0 +1,38 @@
+package q33_搜索旋转排序数组;
+
+/**
+ * @Author: Young
+ * @Date: 2023/12/26 23:56
+ */
+public class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
