890bb1e9713cc422a8fd1fc50d8c7b872ae6870c
Young <young.you.bi@gmail.com>
2024-03-16 15:41:21 +0800

    add q36 Solution

---
+++ src/q36_有效的数独/Solution.java
@@ -0,0 +1,28 @@
+package q36_有效的数独;
+
+/**
+ * @Author: Young
+ * @Date: 2024/3/16 14:52
+ */
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] rows = new int[9][9];
+        int[][] columns = new int[9][9];
+        int[][][] subBoxes = new int[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    int index = c - '0' -1;
+                    rows[i][index]++;
+                    columns[j][index]++;
+                    subBoxes[i / 3][j / 3][index]++;
+                    if (rows[i][index] > 1 || columns[j][index] > 1 || subBoxes[i / 3][j / 3][index] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
