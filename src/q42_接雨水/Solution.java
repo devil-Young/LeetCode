8f8c36d1c8f88161f884d0c1cd891ae85f69ab56
Young <young.you.bi@gmail.com>
2024-04-18 22:07:27 +0800

    q40 Solution

---
+++ src/q42_接雨水/Solution.java
@@ -0,0 +1,33 @@
+package q42_接雨水;
+
+/**
+ * @Author: Young
+ * @Date: 2024/4/18 21:52
+ * 方法一：动态规划
+ */
+public class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i -1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n -1] = height[n -1];
+        for (int i = n - 2; i >= 0 ; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
+}
