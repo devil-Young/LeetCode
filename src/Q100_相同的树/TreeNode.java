de5c06edbf21c7010b7d6e482dd84697049e0861
Young <young.you.bi@gmail.com>
2025-01-05 22:52:44 +0800

    q100 Solution

---
+++ src/Q100_相同的树/TreeNode.java
@@ -0,0 +1,18 @@
+package Q100_相同的树;
+
+/**
+ * @Author: Young
+ * @Date: 2025/1/5 21:52
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
