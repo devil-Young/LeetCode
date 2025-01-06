603e29dd04ac320c4cfdeb165bb072f1d1d78f5d
Young <young.you.bi@gmail.com>
2025-01-06 23:14:31 +0800

    q94 Solution

---
+++ src/q94_二叉树的中序遍历/Solution2.java
@@ -0,0 +1,32 @@
+package q94_二叉树的中序遍历;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: Young
+ * @Date: 2025/1/6 22:55
+ * 迭代
+ * 的递归函数我们也可以用迭代的方式实现，两种方式是等价的，区别在于递归的时候隐式地维护了一个栈，
+ * 而我们在迭代的时候需要显式地将这个栈模拟出来，其他都相同，具体实现可以看下面的代码。
+
+ */
+public class Solution2 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> stk = new LinkedList<TreeNode>();
+        while (root != null || !stk.isEmpty()){
+            while (root != null){
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
---
+++ src/q94_二叉树的中序遍历/TreeNode.java
@@ -0,0 +1,18 @@
+package q94_二叉树的中序遍历;
+
+/**
+ * @Author: Young
+ * @Date: 2025/1/5 21:52
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
