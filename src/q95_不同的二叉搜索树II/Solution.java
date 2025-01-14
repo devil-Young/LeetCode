e9b0d6b735303d7beeebd5016d5f1b084aae69a6
Young <young.you.bi@gmail.com>
2025-01-14 23:31:17 +0800

    q95 Solution

---
+++ src/q95_不同的二叉搜索树II/Solution.java
@@ -0,0 +1,44 @@
+package q95_不同的二叉搜索树II;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: Young
+ * @Date: 2025/1/14 23:14
+ */
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<TreeNode>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            // 获得所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode curTree = new TreeNode(i);
+                    curTree.left = left;
+                    curTree.right = right;
+                    allTrees.add(curTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+
+}
