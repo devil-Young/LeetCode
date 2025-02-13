b596102d527b70e994b162a27ded40a46a98c519
Young <young.you.bi@gmail.com>
2025-02-13 16:50:50 +0800

    add q160

--- src/q160_相交链表/Solution2.java
+++ src/q160_相交链表/Solution2.java
@@ -3,5 +3,17 @@ package q160_相交链表;
 //双指针
 // 空间复杂度 o(1)
 public class Solution2 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null){
+            return null;
+        }
+        ListNode pA = headA;
+        ListNode PB = headB;
+        while (pA != PB){
+            pA = pA == null ? headB : pA.next;
+            PB = PB == null ? headA : PB.next;
+        }
 
+        return null;
+    }
 }
