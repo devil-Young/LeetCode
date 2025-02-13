3f2443a398b0a08a095c4ee8ff420d36fb782560
Young <young.you.bi@gmail.com>
2025-02-13 16:43:03 +0800

    add q160

---
+++ src/q160_相交链表/Solution.java
@@ -0,0 +1,25 @@
+package q160_相交链表;
+
+import java.util.HashSet;
+
+// 时间复杂度：O(m+n)
+//  空间复杂度：O(m)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        HashSet<ListNode> visited = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
---
+++ src/q160_相交链表/Solution2.java
@@ -0,0 +1,7 @@
+package q160_相交链表;
+
+//双指针
+// 空间复杂度 o(1)
+public class Solution2 {
+
+}
