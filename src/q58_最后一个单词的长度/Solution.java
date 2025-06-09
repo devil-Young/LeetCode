62e538e093d8d03bd87fc6513b109c9ce9298976
Young <young.you.bi@gmail.com>
2025-06-10 01:57:03 +0800

    add q58 solution

---
+++ src/q58_最后一个单词的长度/Solution.java
@@ -0,0 +1,26 @@
+package q58_最后一个单词的长度;
+
+/**
+ * @Author: Young
+ * @Date: 2025/6/10 01:44
+ * 方法一：反向遍历
+ */
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        // 去除末尾空格
+        while (s.charAt(index) == ' ') {
+            index--;
+        }
+
+        int wordLength = 0;
+        while (index >= 0 && s.charAt(index) != ' ') {
+            wordLength++;
+            index--;
+        }
+
+        return wordLength;
+
+    }
+
+}
