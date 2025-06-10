370e2530ac37c77e815ceaad13bedf9651b66a4d
Young <young.you.bi@gmail.com>
2025-06-10 17:00:00 +0800

    add q54 solution

--- src/q54_螺旋矩阵/Solution.java
+++ src/q54_螺旋矩阵/Solution.java
@@ -18,12 +18,23 @@ public class Solution {
         boolean[][] visited = new boolean[rows][columns];
         int total = rows * columns;
         int row = 0, column = 0;
-        int [][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 方向数组
         int directionIndex = 0;
         for (int i = 0; i < total; i++) {
             order.add(matrix[row][column]);
             visited[row][column] = true;
-            int newRow = row + directions[directionIndex][0];
+            int nextRow = row + directions[directionIndex][0];
+            int nextColumn = column + directions[directionIndex][1];
+            // 检查下一个位置是否越界或已访问 掉头
+            //visited[nextRow][nextColumn]放在最后 只会在行、列都合法时才访问数组，避免 ArrayIndexOutOfBoundsException。
+            if (nextColumn >= columns || nextRow >= rows || nextColumn < 0 || nextRow < 0 || visited[nextRow][nextColumn]) {
+                // 掉头
+                directionIndex = (directionIndex + 1) % 4; // 顺时针转向
+            }
+            row += directions[directionIndex][0];
+            column += directions[directionIndex][1];
+
         }
+        return order;
     }
 }
