29e6a1072dfb62db50095785c54aa67f6cc48139
Young <young.you.bi@gmail.com>
2025-06-17 00:45:43 +0800

    add q148 solution

---
+++ src/q148_排序链表/Solution.java
@@ -0,0 +1,58 @@
+package q148_排序链表;
+
+
+/**
+ * @Author: Young
+ * @Date: 2025/6/13 15:27
+ * <p>
+ * 方法一：自顶向下归并排序
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // 找到链表的中点
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+
+    }
+
+    // 快慢指针找到中点，并断开链表
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null; // 用于断开链表
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (prev != null) {
+            prev.next = null; // 断开链表
+        }
+        return slow;
+    }
+
+    // 合并两个有序链表
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0), tail = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (l1 != null) ? l1 : l2; // 连接剩余部分
+        return dummy.next;
+    }
+}
