62b2863663e9e4ce7283507c7b4a0c98479a4af2
Young <young.you.bi@gmail.com>
2025-06-17 21:27:39 +0800

    add q146 solution2

--- src/q146_LRU缓存/LRUCache2.java
+++ src/q146_LRU缓存/LRUCache2.java
@@ -15,17 +15,11 @@ public class LRUCache2 {
         int value;
         DLinkedNode prev;
         DLinkedNode next;
-
-        public DLinkedNode() {
-        }
-
-        public DLinkedNode(int _key, int _value) {
-            key = _key;
-            value = _value;
-        }
+        public DLinkedNode() {}
+        public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
     }
 
-    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    private Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
     private int size;
     private int capacity;
     private DLinkedNode head, tail;
@@ -33,8 +27,9 @@ public class LRUCache2 {
     public LRUCache2(int capacity) {
         this.size = 0;
         this.capacity = capacity;
-        this.head = new DLinkedNode();
-        this.tail = new DLinkedNode();
+        // 使用伪头部和伪尾部节点
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
         head.next = tail;
         tail.prev = head;
     }
@@ -42,7 +37,7 @@ public class LRUCache2 {
     public int get(int key) {
         DLinkedNode node = cache.get(key);
         if (node == null) {
-            return -1; // 如果不存在，返回 -1
+            return -1;
         }
         // 如果 key 存在，先通过哈希表定位，再移到头部
         moveToHead(node);
@@ -56,31 +51,22 @@ public class LRUCache2 {
             DLinkedNode newNode = new DLinkedNode(key, value);
             // 添加进哈希表
             cache.put(key, newNode);
-            // 添加到双向链表的头部
+            // 添加至双向链表的头部
             addToHead(newNode);
-            size++;
+            ++size;
             if (size > capacity) {
                 // 如果超出容量，删除双向链表的尾部节点
                 DLinkedNode tail = removeTail();
+                // 删除哈希表中对应的项
                 cache.remove(tail.key);
                 --size;
             }
-        } else {
-            // 如果 key 存在，先更新值，再移到头部
+        }
+        else {
+            // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
             node.value = value;
             moveToHead(node);
         }
-    }
-
-    private DLinkedNode removeTail() {
-        DLinkedNode res = tail.prev;
-        removeNode(res);
-        return res;
-    }
-
-    private void moveToHead(DLinkedNode node) {
-        removeNode(node);
-        addToHead(node);
     }
 
     private void addToHead(DLinkedNode node) {
@@ -88,12 +74,23 @@ public class LRUCache2 {
         node.next = head.next;
         head.next.prev = node;
         head.next = node;
-
     }
 
     private void removeNode(DLinkedNode node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
+
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private DLinkedNode removeTail() {
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
 }
+
 
