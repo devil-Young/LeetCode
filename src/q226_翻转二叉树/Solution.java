0406610b87d12e949332a239f988450f055ff83f
Young <young.you.bi@gmail.com>
2025-06-19 22:49:29 +0800

    add q226 solution

---
+++ src/q226_翻转二叉树/Solution.java
@@ -0,0 +1,19 @@
+package q226_翻转二叉树;
+
+/**
+ * @Author: Young
+ * @Date: 2025/6/19 22:37
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null){
+            return  null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
