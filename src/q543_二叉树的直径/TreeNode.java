7146eb28ad38ecc2e3a75e625db9f433c946ccbf
Young <young.you.bi@gmail.com>
2025-06-20 00:58:22 +0800

    add q543 solution

---
+++ src/q543_二叉树的直径/TreeNode.java
@@ -0,0 +1,27 @@
+package q543_二叉树的直径;
+
+/**
+ * @Author: Young
+ * @Date: 2025/6/20 00:38
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
