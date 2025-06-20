7619c7111e9818662bd8f80057006931fa3c2eed
Young <young.you.bi@gmail.com>
2025-06-20 20:54:22 +0800

    add q102 solution

---
+++ src/q102_二叉树的层序遍历/TreeNode.java
@@ -0,0 +1,24 @@
+package q102_二叉树的层序遍历;
+
+/**
+ * @Author: Young
+ * @Date: 2025/6/20 00:59
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
