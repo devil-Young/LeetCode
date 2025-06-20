10a72bd66386157b60f2774b2a4a5cc12fb28f8e
Young <young.you.bi@gmail.com>
2025-06-20 22:43:49 +0800

    add q1382 solution

---
+++ src/q1382_将二叉搜索树变平衡/TreeNode.java
@@ -0,0 +1,27 @@
+package q1382_将二叉搜索树变平衡;
+
+/**
+ * @Author: Young
+ * @Date: 2025/6/20 22:21
+ */
+
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
